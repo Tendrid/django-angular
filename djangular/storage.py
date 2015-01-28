a00a66ff255c8faf74b97d93d6be05db9c6f3c9c
Tendrid <tendrid@gmail.com>
2015-01-28 13:23:40 -0500

    re

--- djangular/storage.py
+++ djangular/storage.py
@@ -3,13 +3,16 @@ import os
 from re import sub
 
 class AngularAppStorage(AppStaticStorage):
-   source_dir = 'app'
+    source_dir = 'app'
 
-   def __init__(self, app, *args, **kwargs):
-       # app is the actual app module
-       self.prefix = os.path.join(*(app.split('.')))
-       super(AppStaticStorage, self).__init__(app, *args, **kwargs)
+    def __init__(self, app, *args, **kwargs):
+        # app is the actual app module
+        raise TypeError(app)
+        self.prefix = os.path.join(*(app.split('.')))
+        super(AppStaticStorage, self).__init__(app, *args, **kwargs)
 
-   def path(self, name):
-       name = sub('^' + self.prefix + os.sep, '', name)
-       return super(AppStaticStorage, self).path(name)
+    def path(self, name):
+        raise TypeError(name)
+
+        name = sub('^' + self.prefix + os.sep, '', name)
+        return super(AppStaticStorage, self).path(name)
